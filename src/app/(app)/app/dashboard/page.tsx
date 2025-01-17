7286c2e48983c897ed8490e213e7bed872b7a026
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-01-17 18:49:30 +0100

    Add Stats and Branding components

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,21 +1,13 @@
+import Branding from '@/components/branding'
 import H1 from '@/components/h1'
+import Stats from '@/components/stats'
 
 export default function DashboardPage() {
 	return (
 		<main>
 			<div className="flex justify-between items-center text-white py-8">
-				<section>
-					<H1>
-						Pet<span className="font-semibold">Soft</span>
-					</H1>
-					<p className="text-lg opacity-80">
-						Manage your pet daycare with ease
-					</p>
-				</section>
-				<section className="text-center">
-					<p className="text-2xl font-bold leading-6">2</p>
-					<p className="opacity-80">current guests</p>
-				</section>
+				<Branding />
+				<Stats />
 			</div>
 		</main>
 	)
