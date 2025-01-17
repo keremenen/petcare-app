3a7590a7a520dffae45270b53d2980c0d89cc259
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-01-17 18:48:14 +0100

    Add H1 component

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,3 +1,22 @@
+import H1 from '@/components/h1'
+
 export default function DashboardPage() {
-	return <p>Dashboard page</p>
+	return (
+		<main>
+			<div className="flex justify-between items-center text-white py-8">
+				<section>
+					<H1>
+						Pet<span className="font-semibold">Soft</span>
+					</H1>
+					<p className="text-lg opacity-80">
+						Manage your pet daycare with ease
+					</p>
+				</section>
+				<section className="text-center">
+					<p className="text-2xl font-bold leading-6">2</p>
+					<p className="opacity-80">current guests</p>
+				</section>
+			</div>
+		</main>
+	)
 }
