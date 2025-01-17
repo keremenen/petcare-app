7036af2cdc9b4be0d4c818a48fb473a5d5c962cf
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-01-17 18:55:28 +0100

    WIP add dashboard page content

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,5 +1,4 @@
 import Branding from '@/components/branding'
-
 import Stats from '@/components/stats'
 
 export default function DashboardPage() {
@@ -8,6 +7,17 @@ export default function DashboardPage() {
 			<div className="flex justify-between items-center text-white py-8">
 				<Branding />
 				<Stats />
+			</div>
+
+			<div>
+				<SearchForm />
+
+				<ContentBlock>
+					<PetList />
+				</ContentBlock>
+				<ContentBlock>
+					<PetDetails />
+				</ContentBlock>
 			</div>
 		</main>
 	)
