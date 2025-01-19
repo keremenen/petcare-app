8278cd8f159532c338d55afa394ea78752aac6b8
keremenen <131199812+keremenen@users.noreply.github.com>
2025-01-19 21:58:51 +0100

    Update app-footer.tsx

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -1,7 +1,7 @@
 export default function AppFooter() {
 	return (
 		<footer className="border-t border-black/5 py-5 mt-auto">
-			<small className="opacity-50">
+			<small className="opacity-40">
 				&copy; 2025 by Kemer, all rights reserved.
 			</small>
 		</footer>
