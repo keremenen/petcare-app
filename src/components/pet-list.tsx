4c480abe5a5a5dec0c871a8192c661f6baa8c063
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-01-20 19:19:25 +0100

    Add selectedPet button styling

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,9 +1,10 @@
 "use client"
 import { usePetContext } from "@/lib/hooks"
+import { cn } from "@/lib/utils"
 import Image from "next/image"
 
 export default function PetList() {
-  const { pets, handleSetSelectedPetId } = usePetContext()
+  const { pets, handleSetSelectedPetId, selectedPetId } = usePetContext()
 
   return (
     <ul className="border-b border-black/[0.08] bg-white">
@@ -11,7 +12,12 @@ export default function PetList() {
         <li key={pet.id}>
           <button
             onClick={() => handleSetSelectedPetId(pet.id)}
-            className="flex h-[70px] w-full cursor-pointer items-center gap-3 px-5 text-base transition hover:bg-[#EFF1F2] focus:bg-[#EFF1F2]"
+            className={cn(
+              "flex h-[70px] w-full cursor-pointer items-center gap-3 px-5 text-base transition hover:bg-[#EFF1F2] focus:bg-[#EFF1F2]",
+              {
+                "bg-[#EFF1F2]": selectedPetId === pet.id,
+              },
+            )}
           >
             <Image
               src={pet.imageUrl}
