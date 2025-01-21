0274c51ac1c4c5b72ab88f6f9767ebaccaeb6f87
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-01-21 20:01:45 +0100

    (wip) add form structure

--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -1,3 +1,30 @@
+import { Input } from "./ui/input"
+import { Label } from "./ui/label"
+
 export default function PetForm() {
-  return <div>petForm</div>
+  return (
+    <form className="space-y-4">
+      <div className="space-y-2">
+        <Label htmlFor="name">Name</Label>
+        <Input id="name" type="text" />
+      </div>
+
+      <div>
+        <Label htmlFor="ownerName">Owner name</Label>
+        <Input id="ownerName" type="text" />
+      </div>
+      <div>
+        <Label htmlFor="imageUrl">Image URL</Label>
+        <Input id="imageUrl" type="text" />
+      </div>
+      <div>
+        <Label htmlFor="age">Age</Label>
+        <Input id="age" type="number" />
+      </div>
+      <div>
+        <Label htmlFor="ownerName">Notes</Label>
+        <Input id="ownerName" type="text" />
+      </div>
+    </form>
+  )
 }
