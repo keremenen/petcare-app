700a6f8f6b72790a1b96b80ba2a45e00348831c5
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-01-21 20:09:35 +0100

    Add structure to PetForm

--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -1,30 +1,41 @@
+import { Button } from "./ui/button"
 import { Input } from "./ui/input"
 import { Label } from "./ui/label"
+import { Textarea } from "./ui/textarea"
 
-export default function PetForm() {
+type PetFormProps = {
+  actionType: "add" | "edit"
+}
+
+export default function PetForm({ actionType }: PetFormProps) {
   return (
-    <form className="space-y-4">
-      <div className="space-y-2">
-        <Label htmlFor="name">Name</Label>
-        <Input id="name" type="text" />
-      </div>
+    <form className="flex flex-col">
+      <div className="space-y-3">
+        <div className="space-y-2">
+          <Label htmlFor="name">Name</Label>
+          <Input id="name" type="text" />
+        </div>
 
-      <div>
-        <Label htmlFor="ownerName">Owner name</Label>
-        <Input id="ownerName" type="text" />
+        <div>
+          <Label htmlFor="ownerName">Owner name</Label>
+          <Input id="ownerName" type="text" />
+        </div>
+        <div>
+          <Label htmlFor="imageUrl">Image URL</Label>
+          <Input id="imageUrl" type="text" />
+        </div>
+        <div>
+          <Label htmlFor="age">Age</Label>
+          <Input id="age" type="number" />
+        </div>
+        <div>
+          <Label htmlFor="notes">Notes</Label>
+          <Textarea id="notes" rows={4} />
+        </div>
       </div>
-      <div>
-        <Label htmlFor="imageUrl">Image URL</Label>
-        <Input id="imageUrl" type="text" />
-      </div>
-      <div>
-        <Label htmlFor="age">Age</Label>
-        <Input id="age" type="number" />
-      </div>
-      <div>
-        <Label htmlFor="ownerName">Notes</Label>
-        <Input id="ownerName" type="text" />
-      </div>
+      <Button type="submit" className="mt-4 self-end">
+        {actionType === "add" ? "Add pet" : "Save changes"}
+      </Button>
     </form>
   )
 }
