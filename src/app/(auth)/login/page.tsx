e099fcf24889e680fbb31656fb67e9979d3b383e
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-01-27 21:51:52 +0100

    Add data to login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,17 @@
+import AuthForm from "@/components/auth-form"
+import H1 from "@/components/h1"
+import Link from "next/link"
+
 export default function LoginPage() {
-  return <p>Login page</p>
+  return (
+    <main>
+      <H1 className="mb-5 text-center">Log in</H1>
+
+      <AuthForm />
+      <p className="mt-6 text-sm text-zinc-500">
+        Don't have an account?
+        <Link href="/auth/signup">Signup</Link>
+      </p>
+    </main>
+  )
 }
