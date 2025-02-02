f742b48369c5ac6b6e0cb43267b38f0ae4af19c5
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-02-02 13:40:47 +0100

    Add signup validation

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -23,15 +23,26 @@ export async function logIn(formData: unknown) {
   await signIn("credentials", formData)
 }
 
-export async function signUp(formData: FormData) {
-  const hashedPassword = await bcrypt.hash(
-    formData.get("password") as string,
-    10,
-  )
+export async function signUp(formData: unknown) {
+  if (!(formData instanceof FormData)) {
+    return { message: "Invalid credentials" }
+  }
+
+  const formDataFromEntries = Object.fromEntries(formData.entries())
+
+  const validatedFormData = authSchema.safeParse(formDataFromEntries)
+
+  if (!validatedFormData.success) {
+    return { message: "Invalid credentials" }
+  }
+
+  const { email, password } = validatedFormData.data
+
+  const hashedPassword = await bcrypt.hash(password, 10)
 
   await prisma.user.create({
     data: {
-      email: formData.get("email") as string,
+      email,
       hashedPassword,
     },
   })
