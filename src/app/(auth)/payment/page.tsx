370fc357036119f8c1a5118444b80fef25b2a459
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-02-03 20:45:09 +0100

    Add search params to payment page

--- src/app/(auth)/payment/page.tsx
+++ src/app/(auth)/payment/page.tsx
@@ -1,20 +1,35 @@
 "use client"
 import { createCheckoutSesstion } from "@/actions/actions"
 import H1 from "@/components/h1"
+import { use } from "react"
 import { Button } from "@/components/ui/button"
 
-export default function PaymentPage() {
+export default function PaymentPage(props: { searchParams }) {
+  const searchParams = use(props.searchParams)
+  console.log(searchParams)
+
   return (
     <main className="flex flex-col items-center gap-y-10">
       <H1>PetSoft access requires payment</H1>
-
-      <Button
-        onClick={async () => {
-          await createCheckoutSesstion()
-        }}
-      >
-        Buy lifetime access for 299$
-      </Button>
+      {!searchParams.success && (
+        <Button
+          onClick={async () => {
+            await createCheckoutSesstion()
+          }}
+        >
+          Buy lifetime access for 299$
+        </Button>
+      )}
+      {searchParams.success && (
+        <p className="text-sm text-green-700">
+          Payment successful! You now have lifetime access to PetSoft
+        </p>
+      )}
+      {searchParams.canceled && (
+        <p className="text-sm text-red-700">
+          Payment canceled! You need to pay to access PetSoft
+        </p>
+      )}
     </main>
   )
 }
