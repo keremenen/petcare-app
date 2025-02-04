a7ed4612d44a63fda9379f6afd1b4da6cdf1b047
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-02-04 19:36:21 +0100

    Add stripe hook veryfication

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -1,19 +1,38 @@
 import { prisma } from "@/lib/db"
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
 export async function POST(request: Request) {
-  const data = await request.json()
+  const body = await request.text()
+  const signature = request.headers.get("stripe-signature")
 
   //veryfiy the hook is from stripe
+  let event
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET,
+    )
+  } catch (error) {
+    console.log("Webhook verification failed")
+    return Response.json(null, { status: 400 })
+  }
 
   //fulfill the order
-  await prisma.user.update({
-    where: {
-      email: data.data.object.customer_email,
-    },
-    data: {
-      hasAccess: true,
-    },
-  })
 
+  switch (event.type) {
+    case "checkout.session.completed":
+      await prisma.user.update({
+        where: {
+          email: event.data.object.customer_email,
+        },
+        data: {
+          hasAccess: true,
+        },
+      })
+      break
+    default:
+      console.log(`Unhandled event type ${event.type}`)
+  }
   return Response.json(null, { status: 200 })
 }
