4916b289a004f129d004f36cc143110de8b17305
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-02-04 18:08:28 +0100

    Add prisma update on API call

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -1,6 +1,19 @@
+import { prisma } from "@/lib/db"
+
 export async function POST(request: Request) {
-  const body = await request.text()
-  console.log(body)
+  const data = await request.json()
+
+  //veryfiy the hook is from stripe
+
+  //fulfill the order
+  await prisma.user.update({
+    where: {
+      email: data.data.object.customer_email,
+    },
+    data: {
+      hasAccess: true,
+    },
+  })
 
   return Response.json(null, { status: 200 })
 }
